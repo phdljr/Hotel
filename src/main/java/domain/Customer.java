4740e226e7474ff8573133e80a5e70d6887a1ea4
phdljr <phdljr@naver.com>
2023-10-25 12:28:17 +0900

    feat: add Customer field

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,5 +1,37 @@
 package domain;
 
 public class Customer {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private long money;
+    private CustomerType role;
 
+    public Customer(String id, String name, String phoneNumber, long money, CustomerType role) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.money = money;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public CustomerType getCustomerType() {
+        return role;
+    }
 }
